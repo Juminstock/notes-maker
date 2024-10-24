bf014e22fb64df50268b8f2938228cb682631d2c
Carlos Rodríguez <juminstock@gmail.com>
2024-10-23 22:47:30 -0500

    Update: initial state

--- src/App.js
+++ src/App.js
@@ -3,27 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-
       <header className="App-header">
         <p>
           Hello, world!
         </p>
       </header>
     </div>
-  );
-}
-
-function TodoItem() {
-  return (
-    <li>
-      <span>V</span>
-      <p>Llorar con la Llorona</p>
-      <span>X</span>
-    </li>
   );
 }
 
