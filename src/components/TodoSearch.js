c48a058bd831fa24bcf9f35efba637597ce24b93
Carlos Rodríguez <juminstock@gmail.com>
2024-10-27 11:27:24 -0500

    Update: add useState function

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
+
 function TodoSearch() {
+
+    const [searchValue, setSearchValue] = React.useState();
+    useState('');
+
+    console.log("Los usuarios buscan " + searchValue);
+
     return (
         <input 
             placeholder="Cortar cebolla" 
             className="TodoSearch"
+            value={ searchValue }
             onChange={(event) => {
-                console.log(event.target.value)
+                setSearchValue(event.target.value);
             }}
         />
     );
