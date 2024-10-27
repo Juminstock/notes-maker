de3328e7796dbf49be11fbf253a6538fdc98f015
Carlos Rodríguez <juminstock@gmail.com>
2024-10-26 19:43:35 -0500

    update: add styles and font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import './App.css';
+import React from 'react';
 import { TodoItem } from './components/TodoItem';
 import { TodoCounter } from './components/TodoCounter';
 import { TodoSearch } from './components/TodoSearch';
 import { CreateTodoButtom } from './components/CreateTodoButtom';
 import { TodoList } from './components/TodoList';
 
+const defaultTodos = [
+  { text: 'Cortal cebolla', completed: false  },
+  { text: 'Besar a Yilis', completed: false  },
+  { text: 'Pasear a Lion', completed: false  },
+  { text: 'Hacer comida', completed: false  },
+  { text: 'Hacer ejercicio', completed: false  },
+  { text: 'Viajar a Europa', completed: false  }
+];
+
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
 
-      <TodoCounter />
+      <TodoCounter completed={ 0 } total={ defaultTodos.length }/>
       <TodoSearch />
 
       <TodoList>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
       </TodoList>
 
       <CreateTodoButtom />
-    </div>
+    </React.Fragment>
   );
 }
 
